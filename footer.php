812e89169359209e685490a5bd2c0d62046618c6
jemandez <jeman.400@gmail.com>
2016-03-26 06:17:44 -0600

    Nuevo footer

--- footer.php
+++ footer.php
@@ -1,6 +1,26 @@
-<div id="footer">
-<h1>FOOTER</h1>
-</div>
-</div>
-</body>
+    <div id="footer">
+      <div class=" class="row"">
+        <div class="col-lg-4">
+          Necesitas ayuda? Contactanos en:
+
+        </div>
+        <div class="col-lg-4">
+          <i></i>
+        </div>
+        <div class="col-lg-4 center-xs">
+          <h3>Encuentranos en:</h3>
+          <ul class="contactbar row around-xs">
+            <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-facebook-official"></i></a></li>
+            <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="pie" class="row around-xs">
+        <p class="">©2016 Astrokosmos. Todos los derechos reservados.</p> <p>
+          Desarrollado con <i class="fa fa-heart"></i> por: <a href="#">Monitosdigitales.com</a>
+        </p>
+      </div>
+    </div>
+  </body>
 </html>
