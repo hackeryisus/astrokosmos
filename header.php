863314811f9bb040a5bcbe93941beb7eb8fdebe1
jemandez <jeman.400@gmail.com>
2016-03-26 05:38:57 -0600

    Header listo Se creo edito el header junto con sus estilos css

--- header.php
+++ header.php
@@ -1,10 +1,37 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
-<title>Astrokosmos</title>
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <meta charset="UTF-8">
+  <title>Astrokosmos</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <script type="text/javascript">
+    document.addEventListener( "DOMContentLoaded", function(){
+      var btnMenu = document.getElementById('btnMenu');
+      var navbarMenu = document.getElementById('navbarMenu');
+
+      btnMenu.addEventListener('click', onClickMenu);
+
+      function onClickMenu() {
+          navbarMenu.classList.toggle('header-menu-list--show');
+      }
+    });
+  </script>
 </head>
 <body>
-<div id="wrapper">
-<div id="header">
-<h1>HEADER</h1>
-</div>
+
+<header class="header">
+  <div class="row around-xs">
+    <h1 class="header-logo">Astrokosmos</h1>
+    <button id="btnMenu" class="header-btn-nav"><i class="fa fa-bars"></i></button>
+  </div>
+  <nav class="header-menu-navbar">
+    <ul id="navbarMenu" class="header-menu-list">
+      <li class="header-menu-list-item"><a href="#home">Inicio</a></li>
+      <li class="header-menu-list-item"><a href="#about">Productos</a></li>
+      <li class="header-menu-list-item"> <a href="#portfolio">Acerca de</a></li>
+      <li class="header-menu-list-item"><a href="#contact">Contacto</a></li>
+    </ul>
+  </nav>
+</header>
--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php get_header(); ?>
 <div id="main">
   <div id="content">
-    <h1>Main Area</h1>
+    <h1>Nuevos productos</h1>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <h1><?php the_title(); ?></h1>
       <h4>Posted on <?php the_time('F jS, Y') ?></h4>
-      <p><?php the_content(__('(more...)')); ?></p>
+      <p><?php the_content(__('(mas...)')); ?></p>
       <hr> <?php endwhile; else: ?>
-        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+        <p><?php _e('Lo sentimos, ningun producto coincide con los criterios.'); ?></p><?php endif; ?>
   </div>
-<?php get_sidebar(); ?>
 </div>
-
-<div id="delimiter">
-</div>
-
 <?php get_footer(); ?>
