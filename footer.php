746e68048b147b281be97518c074a01f703339ee
jemandez <jeman.400@gmail.com>
2016-04-04 00:47:56 -0500

    Mejoras en los estilos del footer, marcas y categorias

--- footer.php
+++ footer.php
@@ -1,27 +1,31 @@
     <div id="footer">
       <div class="row">
-        <div class="footer-newsletter col-xs-12 col-lg-4">
+        <div class="footer-newsletter col-xs-12 col-sm-6 col-lg-4">
           <h3>Astrokosmos Ofertas</h3>
           <p>¿Quieres recibir informacion acerca de nuestras ofertas, nuevos productos, y promociones?</p>
           <input type="text" name="name" value="Introduce tu email">
           <button type="button" name="send">Enviar</button>
         </div>
-        <div class="footer-findus col-lg-4 center-xs">
+        <div class="footer-findus col-xs-12 col-sm-6 col-lg-4 center-xs">
           <h3>Encuentranos en:</h3>
           <ul class="contactbar row around-xs">
             <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-facebook-official"></i></a></li>
-            <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
             <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
-        <div class="footer-direcction col-xs-12 col-lg-4">
+        <div class="footer-direcction col-xs-12 col-lg-4 center-xs start-md">
           <p>Calle Margaritas 817-E Colonia Bugambilias 72440 Puebla, Puebla, Mexico</p>
           <p>Lun - Vier 9-8 / Sab 10-6</p>
           <p>contacto@astrokosmos.com</p>
         </div>
       </div>
-      <div id="pie" class="fotter-copyright row around-xs">
-        <p class="">©2016 Astrokosmos. Todos los derechos reservados.</p> <p>
+      <div id="footer-copyright" class="row around-xs">
+        <p class="">©2016 Astrokosmos. Todos los derechos reservados.</p>
+        <p>
+          Website layout, design elements, content, and all product designs are the property of Astrokosmos S.A..
+        and may not be used for any purpose without express written consent of Astrokosmos S.A.
+        </p>
+        <p>
           Desarrollado con <i class="fa fa-heart"></i> por: <a href="#">Monitosdigitales.com</a>
         </p>
       </div>
--- index.php
+++ index.php
@@ -2,37 +2,34 @@
 <div class="slider center-sm">
   <img class="slider-image" src="http://www.meade.com/media/easybanner/LightBridge_Sale_Homepage.jpg" alt="" />
 </div>
-<div class="highlightProducts">
+<div id="highlightProducts" class="home-section center-xs">
   <h3>Nuestros Productos</h3>
   <div class="row center-sm bottom-sm">
-    <div class="col-xs-8 col-sm-4">
-      <img class="responsiveimage" src="http://d243u7pon29hni.cloudfront.net/images/products/Telescopio_bresser_8840002_50-600_50x-100x1_l.png" alt="" />
+    <div class="home-section-item col-xs-8 col-sm-4">
+      <img class="" src="http://d243u7pon29hni.cloudfront.net/images/products/Telescopio_bresser_8840002_50-600_50x-100x1_l.png" alt="" />
       <p>Telescopios</p>
     </div>
-    <div class="col-xs-8 col-sm-4">
-      <img class="responsiveimage" src="http://ecx.images-amazon.com/images/I/71F8UGS4UDL._SL1200_.jpg" alt="" />
+    <div class="home-section-item col-xs-8 col-sm-4">
+      <img class="" src="http://ecx.images-amazon.com/images/I/71F8UGS4UDL._SL1200_.jpg" alt="" />
       <p>Binoculares</p>
     </div>
-    <div class="col-xs-8 col-sm-4">
-      <img class="responsiveimage" src="http://www.microscope.com/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/o/m/omano_om118_m3_monocular_compound_microscope_1.png" alt="" />
+    <div class="home-section-item col-xs-8 col-sm-4">
+      <img class="" src="http://www.microscope.com/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/o/m/omano_om118_m3_monocular_compound_microscope_1.png" alt="" />
       <p>Equipo optico</p>
     </div>
   </div>
 </div>
-<div class="brands">
+<div id="brands" class="home-section center-xs">
   <h3>Las marcas que nos respaldan</h3>
   <div class="row center-sm bottom-sm">
-    <div class="col-xs-8 col-sm-4">
-      <img class="responsiveimage" src="http://images4.wikia.nocookie.net/__cb20121104001423/logopedia/images/e/ee/Burger_King_Logo.svg.png" alt="" />
-      <p>Telescopios</p>
+    <div class="home-section-item col-xs-8 col-sm-4">
+      <img class="" src="http://images4.wikia.nocookie.net/__cb20121104001423/logopedia/images/e/ee/Burger_King_Logo.svg.png" alt="" />
     </div>
-    <div class="col-xs-8 col-sm-4">
-      <img class="responsiveimage" src="https://s3.amazonaws.com/uploads.startups.fm/wp-content/uploads/2013/11/starbucks-logo.png" alt="" />
-      <p>Binoculares</p>
+    <div class="home-section-item col-xs-8 col-sm-4">
+      <img class="" src="https://s3.amazonaws.com/uploads.startups.fm/wp-content/uploads/2013/11/starbucks-logo.png" alt="" />
     </div>
-    <div class="col-xs-12 col-sm-4">
-      <img class="responsiveimage" src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Danone_dairy_brand_logo.svg/1280px-Danone_dairy_brand_logo.svg.png" alt="" />
-      <p>Equipo optico</p>
+    <div class="home-section-item col-xs-12 col-sm-4">
+      <img class="" src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Danone_dairy_brand_logo.svg/1280px-Danone_dairy_brand_logo.svg.png" alt="" />
     </div>
   </div>
 </div>
