ef9933e8b667bfee1020b404cbb904e2beedff82
jemandez <jeman.400@gmail.com>
2016-04-03 23:11:22 -0500

    Pequeños cambios en el diseño del header

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <title>Astrokosmos</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" />
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <script type="text/javascript">
     document.addEventListener( "DOMContentLoaded", function(){
@@ -24,7 +25,7 @@
 <header class="header">
   <div class="row">
     <div class="col-xs-9 col-sm-6">
-      <h1 class="header-logo">Astrokosmos</h1>
+      <h1 id="title" class="header-logo">Astrokosmos</h1>
       <p class="header-slogan">Diseño, tecnología y observación</p>
     </div>
     <button id="btnMenu" class="header-btn-nav col-xs-3"><i class="fa fa-bars"></i></button>
