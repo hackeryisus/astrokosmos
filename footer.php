af73652ac438b04f709f31a838a008c2bf7c47c3
jemandez <jeman.400@gmail.com>
2016-04-04 23:01:45 -0500

    Se hicieron ajustes menores en los elementos del footer y body

--- footer.php
+++ footer.php
@@ -6,14 +6,14 @@
           <input type="text" name="name" value="Introduce tu email">
           <button type="button" name="send">Enviar</button>
         </div>
-        <div class="footer-findus col-xs-12 col-sm-6 col-lg-4 center-xs">
+        <div id="footer-findus" class=" col-xs-12 col-sm-6 col-lg-4 center-xs">
           <h3>Encuentranos en:</h3>
           <ul class="contactbar row around-xs">
             <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-facebook-official"></i></a></li>
             <li class="contactbar-item"><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
-        <div class="footer-direcction col-xs-12 col-lg-4 center-xs start-md">
+        <div id="footer-direcction" class="col-xs-12 col-lg-4 center-xs start-md">
           <p>Calle Margaritas 817-E Colonia Bugambilias 72440 Puebla, Puebla, Mexico</p>
           <p>Lun - Vier 9-8 / Sab 10-6</p>
           <p>contacto@astrokosmos.com</p>
@@ -21,13 +21,8 @@
       </div>
       <div id="footer-copyright" class="row around-xs">
         <p class="">©2016 Astrokosmos. Todos los derechos reservados.</p>
-        <p>
-          Website layout, design elements, content, and all product designs are the property of Astrokosmos S.A..
-        and may not be used for any purpose without express written consent of Astrokosmos S.A.
-        </p>
-        <p>
-          Desarrollado con <i class="fa fa-heart"></i> por: <a href="#">Monitosdigitales.com</a>
-        </p>
+        <p>Este sitio, los elementos de diseño, contenido, son propiedad de Astrokosmos y no deben de ser usados para ningun proposito sin el consentimienot previo por escrito de Astrokosmos</p>
+        <p>Desarrollado con <i class="fa fa-heart"></i> por: <a href="#">Monitosdigitales.com</a></p>
       </div>
     </div>
   </body>
